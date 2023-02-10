8c38c9798d0e4da97b6ba01315f43ecb366a0d54
Joel B <smol@toast.cyou>
2023-02-10 12:19:12 -0500

    trying a new fix

--- src/generator.rs
+++ src/generator.rs
@@ -46,7 +46,7 @@ pub fn generate(prompt: &str, min_len: i64, max_len: Option<i64>) -> String {
     model.set_device(Device::cuda_if_available());
 
     let input_context_1 = prompt.trim_start_matches("e.ask");
-    let output = model.generate(&[prompt, input_context_1], None);
+    let output: Vec<String> = model.generate(&[prompt, input_context_1], None);
 
     let response: String = output.into_iter().collect().trim_start_matches(PROMPT);
     return response
