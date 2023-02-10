ce217b54ddf432459522e7e47d7ec573eb9ad104
Joel B <smol@toast.cyou>
2023-02-10 12:40:34 -0500

    silly sauce

--- src/main.rs
+++ src/main.rs
@@ -217,7 +217,7 @@ async fn ask(ctx: &Context, msg: &Message) -> CommandResult {
         println!("not dead!");
         // This is running on a thread where blocking is fine.
         let response = format!("{}", generator::generate(
-            &prompt.strip_prefix(&format!("{} e.ask", generator::PROMPT)).unwrap(),
+            &prompt.strip_prefix(&format!("{} e.ask", &generator::PROMPT))?,
             35,
             Some(150)
         ));
