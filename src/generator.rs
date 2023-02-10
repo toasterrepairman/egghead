f84f493318c001eec51c5b39d3aaad892d863a2a
Joel B <smol@toast.cyou>
2023-02-10 12:16:29 -0500

    trying a new fix

--- src/generator.rs
+++ src/generator.rs
@@ -48,9 +48,6 @@ pub fn generate(prompt: &str, min_len: i64, max_len: Option<i64>) -> String {
     let input_context_1 = prompt.trim_start_matches("e.ask");
     let output = model.generate(&[prompt, input_context_1], None);
 
-    let response: String = output
-        .into_iter()
-        .collect()
-        .trim_start_matches(PROMPT);
+    let response: String = output.into_iter().collect().trim_start_matches(PROMPT);
     return response
 }
