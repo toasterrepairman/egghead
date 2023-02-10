a6fe75bcc6bd0fb55bde077211bf5a8604bbec00
Joel B <smol@toast.cyou>
2023-02-10 12:55:53 -0500

    trying a new fix

--- src/generator.rs
+++ src/generator.rs
@@ -45,8 +45,8 @@ pub fn generate(prompt: &str, min_len: i64, max_len: Option<i64>) -> String {
         .expect("Uh oh, it's the generator broken.");
     model.set_device(Device::cuda_if_available());
 
-    let input_context_1 = prompt.starts_with("e.ask");
-    let output: Vec<String> = model.generate(&[prompt, input_context_1], None);
+    let input_context_1 = prompt.strip_prefix(format!("{} e.ask", PROMPT));
+    let output: Vec<String> = model.generate(&[prompt.unwrap(), input_context_1], None);
 
     let response: String = output.into_iter().collect();
     return response
