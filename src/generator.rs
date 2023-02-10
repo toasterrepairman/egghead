fd6d4882c1c7a00b5124b0b06ed757dfc685a24a
Joel B <smol@toast.cyou>
2023-02-10 13:48:02 -0500

    trying a new fix

--- src/generator.rs
+++ src/generator.rs
@@ -46,7 +46,7 @@ pub fn generate(prompt: &str, min_len: i64, max_len: Option<i64>) -> String {
     model.set_device(Device::cuda_if_available());
 
     let input_context_1 = prompt;
-    let output = model.generate(&[PROMPT, input_context_1], None);
+    let output = model.generate(&[PROMPT, input_context_1.split_off(5)], None);
 
     let response: String = output.into_iter().collect();
     return response
