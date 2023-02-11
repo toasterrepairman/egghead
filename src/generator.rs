2896f351c02c1404d3c034a82fe8a6317675d8fb
Joel B <smol@toast.cyou>
2023-02-11 15:23:15 -0500

    improving intelligence

--- src/generator.rs
+++ src/generator.rs
@@ -112,16 +112,16 @@ pub fn code(prompt: &str) -> String {
 
 pub fn gen(prompt: &str) -> String {
     let config_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoConfigResources::GPT_NEO_125M,
+        GptNeoConfigResources::GPT_NEO_1_3B,
     ));
     let vocab_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoVocabResources::GPT_NEO_125M,
+        GptNeoVocabResources::GPT_NEO_1_3B,
     ));
     let merges_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoMergesResources::GPT_NEO_125M,
+        GptNeoMergesResources::GPT_NEO_1_3B,
     ));
     let model_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoModelResources::GPT_NEO_125M,
+        GptNeoModelResources::GPT_NEO_1_3B,
     ));
     let generate_config = TextGenerationConfig {
         model_type: ModelType::GPTNeo,
