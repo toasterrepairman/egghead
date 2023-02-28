d6c86275246a9f34407168f722d7efaf353d4e19
Joel B <smol@toast.cyou>
2023-02-27 20:39:47 -0500

    testing see

--- src/generator.rs
+++ src/generator.rs
@@ -14,11 +14,9 @@ use std::sync::Arc;
 use uuid::{Uuid, Version};
 
 use pyke_diffusers::{
-    Environment, EulerDiscreteScheduler, SchedulerOptimizedDefaults, StableDiffusionOptions, StableDiffusionPipeline,
+    OrtEnvironment, EulerDiscreteScheduler, SchedulerOptimizedDefaults, StableDiffusionOptions, StableDiffusionPipeline,
     StableDiffusionTxt2ImgOptions
 };
-
-
 
 pub fn ask(question: &str, context: &str) -> String {
     let config_resource = Box::new(RemoteResource::from_pretrained(
