aeae585c9e300638756c6af688f25c612ba22f8c
Joel B <smol@toast.cyou>
2023-02-27 22:00:47 -0500

    testing see

--- src/generator.rs
+++ src/generator.rs
@@ -61,7 +61,7 @@ pub fn ask(question: &str, context: &str) -> String {
 }
 
 pub fn look(prompt: &str) -> Result<String, E> {
-    let environment = Environment::builder().with_name("test").with_log_level(LoggingLevel::Verbose).build()?;
+    let environment = OrtEnvironment::builder().with_name("test").with_log_level(LoggingLevel::Verbose).build()?.into_arc();
     let mut scheduler = EulerDiscreteScheduler::stable_diffusion_v1_optimized_default()?;
     let pipeline = StableDiffusionMemoryOptimizedPipeline::new(&environment, "./stable-diffusion-v1-5/", StableDiffusionOptions::default())?;
 
