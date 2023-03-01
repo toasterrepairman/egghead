7e2f6325503019d63b0c2526346a0afe562e58a3
Joel B <smol@toast.cyou>
2023-02-28 19:53:32 -0500

    wiki feature (fixed)

--- src/main.rs
+++ src/main.rs
@@ -307,7 +307,7 @@ async fn wiki(ctx: &Context, msg: &Message) -> CommandResult {
 
     let input = match &msg.content.len() {
         6 => None,
-        _ => Some(msg.content.as_str().split_at(7)[2])
+        _ => Some(msg.content.as_str().split_at(7).1)
     };
 
     let prompt = fetcher::get_wikipedia_summary(input).await.unwrap();
