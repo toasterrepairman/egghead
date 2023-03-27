ae4eccd5a0643e00020d8d1f7a9195159a0e9bf5
Joel B <smol@toast.cyou>
2023-03-27 19:30:23 -0400

    corrected match pattern

--- src/generator.rs
+++ src/generator.rs
@@ -10,12 +10,12 @@ pub fn get_chat_response(prompt: &str) -> Result<String, reqwest::Error> {
     let temperature = "0.7";
     let top_k = "50";
     let top_p = "0.95";
-    let max_length = "2048";
-    let context_window = "1024";
+    let max_length = "1024";
+    let context_window = "512";
     let repeat_last_n = "64";
     let repeat_penalty = "1.3";
     let init_prompt = encode("I am egghead, the world's smartest computer. I will write a response that appropriately completes the request. The response must be accurate, concise and evidence-based whenever possible. A complete answer is always ended by [end of text].");
-    let n_threads = "3";
+    let n_threads = "2";
 
     // Get a chat UUID
     let params = format!("?model={}&temperature={}&top_k={}&top_p={}&max_length={}&context_window={}&repeat_last_n={}&repeat_penalty={}&init_prompt={}&n_threads={}",
@@ -27,10 +27,10 @@ pub fn get_chat_response(prompt: &str) -> Result<String, reqwest::Error> {
     // Make a POST request to api/chat/<UUID>/question with the prompt
     let question_url = format!("{}/{}/question?prompt={}", base_url, uuid, encode(prompt));
     println!("{}", &question_url);
-    Client::new().post(&question_url).timeout(std::time::Duration::from_secs(75)).send()?;
+    Client::new().post(&question_url).timeout(std::time::Duration::from_secs(42)).send()?;
 
     // Block for 60 seconds before returning the response
-    std::thread::sleep(Duration::from_secs(50));
+    std::thread::sleep(Duration::from_secs(40));
 
     let mut response = Client::new().get(format!("{}/{}", base_url, uuid)).send()?;
     let response_text = response.text()?;
