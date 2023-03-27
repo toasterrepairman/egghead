4a14e7a530648705d5262633c5c576148db75eb0
Joel B <smol@toast.cyou>
2023-03-27 18:44:52 -0400

    corrected match pattern

--- src/generator.rs
+++ src/generator.rs
@@ -27,7 +27,7 @@ pub fn get_chat_response(prompt: &str) -> Result<String, reqwest::Error> {
     // Make a POST request to api/chat/<UUID>/question with the prompt
     let question_url = format!("{}/{}/question?prompt={}", base_url, uuid, encode(prompt));
     println!("{}", &question_url);
-    Client::new().post(&question_url).timeout(std::time::Duration::from_secs(60)).send()?;
+    Client::new().post(&question_url).timeout(std::time::Duration::from_secs(90)).send()?;
 
     // Block for 60 seconds before returning the response
     std::thread::sleep(Duration::from_secs(42));
