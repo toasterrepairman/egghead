13904ebba3efca3acd4b086880d287ce63000d91
Joel B <smol@toast.cyou>
2023-03-27 23:46:41 -0400

    corrected init

--- src/main.rs
+++ src/main.rs
@@ -259,7 +259,7 @@ async fn left(ctx: &Context, msg: &Message) -> CommandResult {
     let runner = tokio::task::spawn_blocking(move || {
         println!("Thread Spawned!");
         // This is running on a thread where blocking is fine.
-        let response = generator::get_chat_response("Write an outrageous parody reaction to the following article using the title as a basis for wild assumptions. A complete answer is always ended by [end of text].", &prompt).unwrap();
+        let response = generator::get_chat_response("Write a short parody article based on the following headline. A complete article is always ended by [end of text].", &prompt).unwrap();
         response
     });
 
@@ -285,7 +285,7 @@ async fn right(ctx: &Context, msg: &Message) -> CommandResult {
     let runner = tokio::task::spawn_blocking(move || {
         println!("Thread Spawned!");
         // This is running on a thread where blocking is fine.
-        let response = generator::get_chat_response("Write an outrageous parody reaction to the following article using the title as a basis for wild assumptions. A complete answer is always ended by [end of text].", &prompt).unwrap();
+        let response = generator::get_chat_response("Write a short parody article based on the following headline. A complete article is always ended by [end of text].", &prompt).unwrap();
         response
     });
 
