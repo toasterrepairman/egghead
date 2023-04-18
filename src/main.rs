d26a92be0b05c3a3d8be6b75eb5e434f9c861dcb
Joel B <smol@toast.cyou>
2023-04-17 22:33:41 -0400

    temp control x

--- src/main.rs
+++ src/main.rs
@@ -329,7 +329,7 @@ async fn react(ctx: &Context, msg: &Message) -> CommandResult {
     let heat = if &msg.content.clone().split_off(7).trim().to_string() == "" {
         "1.0"
     } else {
-        &msg.content.clone().split_off(7).trim()
+        &msg.content.clone().split_off(7).trim().clone()
     }.to_string();
 
     println!("{:?}", &heat);
