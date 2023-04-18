0d7cd92195c3e1b86f41dfe2bab45c2beb9058d1
Joel B <smol@toast.cyou>
2023-04-17 22:51:56 -0400

    temp control x

--- src/main.rs
+++ src/main.rs
@@ -349,7 +349,7 @@ async fn react(ctx: &Context, msg: &Message) -> CommandResult {
     let typing: _ = Typing::start(ctx.http.clone(), msg.channel_id.0.clone())
         .expect("Typing failed");
 
-    let input = *&msg.content.clone().split_off(7).trim();
+    let input = &*msg.content.clone().split_off(7).trim();
 
     let heat = if input.to_string() == "" {
         "1.0"
