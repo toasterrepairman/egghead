91121af564f2eae4beba4437a7b984b2d95b14aa
Joel B <smol@toast.cyou>
2023-05-07 14:56:26 -0400

    fixed

--- src/fakeyou.rs
+++ src/fakeyou.rs
@@ -72,7 +72,7 @@ pub async fn get_audio_url(voice_name: &str, message: &str) -> Result<String, Bo
     let mut audio_url = None;
     while audio_url.is_none() {
         sleep(Duration::from_secs(3)).await; // Wait before polling
-        let job_status_url = format!("https://api.fakeyou.com/tts/job/{:?}", job_token.as_ref().unwrap());
+        let job_status_url = format!("https://api.fakeyou.com/tts/job/{}", job_token.as_ref().unwrap());
         println!("{}", &job_status_url);
         let status_response: InferenceJobResponse = client
             .get(&job_status_url)
