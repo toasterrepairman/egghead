0250776d68ca4f21036113036aa00baf9bf7a5f4
Joel B <smol@toast.cyou>
2023-05-10 16:19:04 -0400

    new backend

--- src/generator.rs
+++ src/generator.rs
@@ -2,19 +2,24 @@ use reqwest::Error;
 use serde_json::Value;
 
 pub async fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String, reqwest::Error> {
-    let api_url = "http://localhost:8080/v1/completions";
+    let url = "http://localhost:8080/v1/completions";
+    let headers = reqwest::header::HeaderMap::new();
+    headers.insert("Content-Type", "application/json");
+
     let response = reqwest::Client::new()
-        .post(api_url)
-        .json(&serde_json::json!({
+        .post(url)
+        .headers(headers)
+        .json(&json!({
             "model": "ggml-gpt4all-j.bin",
-            "prompt": prompt,
+            "prompt": format!("{}\n{{\n", init, prompt),
             "temperature": temp.parse::<f64>().unwrap()
         }))
         .send()
         .await?;
 
     let response_json: Value = response.json::<Value>().await?;
-    let text_completion = response_json["choices"][0]["text"].as_str().unwrap();
+    let text_completion = response_json["choices"].as_array().unwrap().first().unwrap();
+    let response_text = text_completion["text"].as_str().unwrap().to_owned();
 
-    Ok(text_completion.to_string())
+    Ok(response_text)
 }
