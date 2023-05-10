10b6a4d794808d5e9f227e90957971439907a371
Joel B <smol@toast.cyou>
2023-05-10 16:53:16 -0400

    new backend

--- src/generator.rs
+++ src/generator.rs
@@ -1,58 +1,18 @@
-use reqwest::Client;
-use serde::{Deserialize, Serialize};
-use std::sync::Mutex;
+use std::io::Read;
+use reqwest::blocking::Client;
 
-lazy_static::lazy_static! {
-    static ref CLIENT: Mutex<Client> = Mutex::new(Client::new());
-}
+pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String, reqwest::Error> {
+    let prompt = format!("{}{}\n", init, prompt);
 
-#[derive(Serialize)]
-struct ChatRequest {
-    model: String,
-    prompt: String,
-    temperature: f32,
-}
+    let client = Client::new();
+    let res = client.post("http://localhost:8080/v1/completions")
+        .header("Content-Type", "application/json")
+        .body(format!(r#"{{"model": "ggml-gpt4all-j.bin", "prompt": "{}", "temperature": {}}}"#, prompt, temp))
+        .send()?
+        .text()?;
 
-#[derive(Deserialize)]
-struct ChatResponse {
-    choices: Vec<ChatChoice>,
-}
+    let json: serde_json::Value = serde_json::from_str(&res)?;
+    let text = json["choices"][0]["text"].as_str().unwrap_or_default();
 
-#[derive(Deserialize)]
-struct ChatChoice {
-    text: String,
-}
-
-pub fn get_chat_response(
-    temp: &str,
-    init: &str,
-    prompt: &str,
-) -> Result<String, reqwest::Error> {
-    let temperature = temp.parse::<f32>().unwrap_or(0.7);
-    let request = ChatRequest {
-        model: init.to_string(),
-        prompt: prompt.to_string(),
-        temperature,
-    };
-
-    let client = CLIENT.lock().unwrap();
-    let response = client
-        .post("http://localhost:8080/v1/completions")
-        .json(&request)
-        .send()
-        .and_then(|resp| resp.json::<ChatResponse>());
-
-    match response {
-        Ok(chat_response) => {
-            if let Some(choice) = chat_response.choices.into_iter().next() {
-                Ok(choice.text)
-            } else {
-                Err(reqwest::Error::new(
-                    reqwest::StatusCode::INTERNAL_SERVER_ERROR,
-                    "No choices returned",
-                ))
-            }
-        }
-        Err(error) => Err(error),
-    }
+    Ok(String::from(text))
 }
