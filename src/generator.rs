7285b54f9596330363d6af818baa4f15529c3db5
Joel B <smol@toast.cyou>
2023-05-10 16:55:01 -0400

    new backend

--- src/generator.rs
+++ src/generator.rs
@@ -1,18 +1,19 @@
-use std::io::Read;
 use reqwest::blocking::Client;
 
 pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String, reqwest::Error> {
-    let prompt = format!("{}{}\n", init, prompt);
-
     let client = Client::new();
-    let res = client.post("http://localhost:8080/v1/completions")
+
+    let payload = format!(
+        r#"{{"model": "ggml-gpt4all-j.bin", "prompt": "{}\n{}\n", "temperature": {}}}"#,
+        init, prompt, temp
+    );
+
+    let response = client
+        .post("http://localhost:8080/v1/completions")
         .header("Content-Type", "application/json")
-        .body(format!(r#"{{"model": "ggml-gpt4all-j.bin", "prompt": "{}", "temperature": {}}}"#, prompt, temp))
+        .body(payload)
         .send()?
         .text()?;
 
-    let json: serde_json::Value = serde_json::from_str(&res)?;
-    let text = json["choices"][0]["text"].as_str().unwrap_or_default();
-
-    Ok(String::from(text))
+    Ok(response)
 }
