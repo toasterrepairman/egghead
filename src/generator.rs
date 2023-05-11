0b1a2d397d96a158e836a04041a85a6395869223
Joel B <smol@toast.cyou>
2023-05-11 15:50:41 -0400

    tracing

--- src/generator.rs
+++ src/generator.rs
@@ -1,46 +1,20 @@
 use reqwest::blocking::Client;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize)]
-struct RequestBody<'a> {
-    model: &'a str,
-    prompt: &'a str,
-    temperature: f64,
-}
-
-#[derive(Deserialize)]
-struct ResponseBody {
-    object: String,
-    model: String,
-    choices: Vec<Choice>,
-    usage: Usage,
-}
-
-#[derive(Deserialize)]
-struct Choice {
-    text: String,
-}
-
-#[derive(Deserialize)]
-struct Usage {
-    prompt_tokens: usize,
-    completion_tokens: usize,
-    total_tokens: usize,
-}
+use std::time::Duration;
 
 pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String, reqwest::Error> {
+    let url = "http://localhost:8080/v1/completions";
+    let input = format!("{}{}\n{}", init, prompt, temp);
     let client = Client::new();
-    let body = RequestBody {
-        model: "ggml-gpt4all-j.bin",
-        prompt: &format!("{}{}\n{}", init, prompt, temp),
-        temperature: 0.7,
-    };
     let response = client
-        .post("http://localhost:8080/v1/completions")
+        .post(url)
         .header("Content-Type", "application/json")
-        .json(&body)
+        .timeout(Duration::from_secs(300))
+        .body(format!(
+            r#"{{"model":"ggml-gpt4all-j.bin","prompt":"{}","temperature":{}}}"#,
+            input, temp
+        ))
         .send()?
-        .json::<ResponseBody>()?;
-    println!("{:?}", response.choices[0].text.clone());
-    Ok(response.choices[0].text.clone())
+        .json::<serde_json::Value>()?;
+    let text = response["choices"][0]["text"].as_str().unwrap();
+    Ok(text.to_owned())
 }
