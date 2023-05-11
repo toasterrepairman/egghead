43260df8980122107bde04e07b8aa522c8bb62fe
Joel B <smol@toast.cyou>
2023-05-11 15:45:17 -0400

    tracing

--- src/generator.rs
+++ src/generator.rs
@@ -41,6 +41,6 @@ pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String,
         .json(&body)
         .send()?
         .json::<ResponseBody>()?;
-    println!("{:?}", response.choices.clone());
+    println!("{:?}", response.choices[0].text.clone());
     Ok(response.choices[0].text.clone())
 }
