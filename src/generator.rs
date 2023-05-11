745fe2bbdc1e97513390a2714081166908d8b80c
Joel B <smol@toast.cyou>
2023-05-11 17:26:24 -0400

    tracing

--- src/generator.rs
+++ src/generator.rs
@@ -10,7 +10,7 @@ pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String,
     // let prompt_input = format!("{}\n{}", init, prompt);
     let request_data = json!({
         "model": "ggml-vic7b-q4_0.bin",
-        "messages": [{"role": init, "content": prompt}]
+        "messages": [{"role": init, "content": prompt}],
         "temperature": temp.parse::<f64>().unwrap()
     });
 
