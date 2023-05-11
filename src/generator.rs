199b02e22ff3fee23edd9b47d6553215f49b36c2
Joel B <smol@toast.cyou>
2023-05-11 16:35:09 -0400

    tracing

--- src/generator.rs
+++ src/generator.rs
@@ -1,20 +1,27 @@
 use reqwest::blocking::Client;
+use serde_json::json;
 use std::time::Duration;
 
 pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String, reqwest::Error> {
-    let url = "http://localhost:8080/v1/completions";
-    let input = format!("{}{}\n{}", init, prompt, temp);
-    let client = Client::new();
-    let response = client
-        .post(url)
-        .header("Content-Type", "application/json")
+    let client = Client::builder()
         .timeout(Duration::from_secs(300))
-        .body(format!(
-            r#"{{"model":"ggml-gpt4all-j.bin","prompt":"{}","temperature":{}}}"#,
-            input, temp
-        ))
-        .send()?
-        .json::<serde_json::Value>()?;
-    let text = response["choices"][0]["text"].as_str().unwrap();
-    Ok(text.to_owned())
+        .build()?;
+
+    let prompt_input = format!("{}\n{}", init, prompt);
+    let request_data = json!({
+        "model": "ggml-gpt4all-j.bin",
+        "prompt": prompt_input,
+        "temperature": temp
+    });
+
+    let response = client
+        .post("http://localhost:8080/v1/completions")
+        .header("Content-Type", "application/json")
+        .json(&request_data)
+        .send()?;
+
+    let response_json: serde_json::Value = response.json()?;
+    let completion_text = response_json["choices"][0]["text"].as_str().unwrap_or("");
+
+    Ok(completion_text.to_string())
 }
