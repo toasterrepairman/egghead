00e5d6b5f6fbe60f0ab3c288026d63a6123d9812
Joel B <smol@toast.cyou>
2023-05-12 23:12:51 -0400

    tuneup

--- src/main.rs
+++ src/main.rs
@@ -236,7 +236,7 @@ async fn ask(ctx: &Context, msg: &Message) -> CommandResult {
     let runner = tokio::task::spawn_blocking(move || {
         println!("Thread Spawned!");
         // This is running on a thread where blocking is fine.
-        let response = generator::get_chat_response("0.6", "I am egghead, the world's smartest computer. I will write a complete answer that is always ended by [end of text].", &prompt).unwrap();
+        let response = generator::get_chat_response("0.6", "I am egghead, the world's smartest computer. You asked me:", &prompt).unwrap();
         response
     });
 
