405fa73ef0395646245e188010c33a25dd539785
Joel B <smol@toast.cyou>
2023-06-13 19:58:50 -0400

    changed model

--- src/generator.rs
+++ src/generator.rs
@@ -37,7 +37,7 @@ pub fn get_code_response(temp: &str, init: &str, prompt: &str) -> Result<String,
 
     let prompt_input = format!("{}{}\n", init, prompt);
     let request_data = json!({
-        "model": "ggml-model-f16.bin",
+        "model": "RedPajama-INCITE-Chat-3B-v1-q4_0.bin",
         "prompt": prompt_input,
         "temperature": temp.parse::<f64>().unwrap(),
         // "stream": false,
