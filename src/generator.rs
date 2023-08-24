2e280ee0003a705d48dca20a0202f6fd8ed508fb
Joel B <smol@toast.cyou>
2023-08-24 13:28:38 -0400

    new model

--- src/generator.rs
+++ src/generator.rs
@@ -30,6 +30,34 @@ pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String,
     Ok(completion_text.to_string())
 }
 
+pub fn get_smart_response(temp: &str, init: &str, prompt: &str) -> Result<String, reqwest::Error> {
+    let client = Client::builder()
+        .timeout(Duration::from_secs(300))
+        .build()?;
+
+    let prompt_input = format!("{}{}\n", init, prompt);
+    let request_data = json!({
+        "model": "wizardlm-1.0-uncensored-llama2-13b.ggmlv3.q4_1.bin",
+        "prompt": prompt_input,
+        "temperature": temp.parse::<f64>().unwrap(),
+        // "stream": false,
+        // "max_tokens": 64,
+    });
+
+    let response = client
+        .post("http://localhost:8080/v1/completions")
+        .header("Content-Type", "application/json")
+        .json(&request_data)
+        .send()?;
+
+    let response_json: serde_json::Value = response.json()?;
+    println!("{}", &response_json);
+    let completion_text = response_json["choices"][0]["text"].as_str().unwrap_or("Prompt machine broke");
+    println!("{}", &completion_text);
+
+    Ok(completion_text.to_string())
+}
+
 pub fn get_code_response(temp: &str, init: &str, prompt: &str) -> Result<String, reqwest::Error> {
     let client = Client::builder()
         .timeout(Duration::from_secs(300))
