2e9e01e8d671538292e7a28156a8acde878ed538
Joel B. <smol@toast.cyou>
2023-12-04 15:01:44 -0500

    unwrap fix

--- src/main.rs
+++ src/main.rs
@@ -582,7 +582,7 @@ async fn j(ctx: &Context, msg: &Message) -> CommandResult {
                 answer
             } else {
                 println!("No clues found");
-                "broken"
+                "broken".to_string()
             }
         } else {
             println!("Failed to fetch data from the API. Status code: {}", response.status());
