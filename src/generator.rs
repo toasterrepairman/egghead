b45765fbd1542ce15f3d5e88ccb46b289ab4c1c8
Joel B. <smol@toast.cyou>
2023-12-22 17:05:50 -0500

    new model

--- src/generator.rs
+++ src/generator.rs
@@ -9,7 +9,7 @@ pub fn get_chat_response(temp: &str, init: &str, prompt: &str) -> Result<String,
 
     let prompt_input = format!("{}{}\n\n", init, prompt);
     let request_data = json!({
-        "model": "orca-mini-3b.ggmlv3.q4_0.bin",
+        "model": "Wizard-Vicuna-7B-Uncensored.ggmlv3.q4_1.bin",
         "prompt": prompt_input,
         "temperature": temp.parse::<f64>().unwrap(),
         "stream": false,
@@ -37,7 +37,7 @@ pub fn get_short_response(temp: &str, init: &str, prompt: &str) -> Result<String
 
     let prompt_input = format!("{}{}\n\n", init, prompt);
     let request_data = json!({
-        "model": "orca-mini-3b.ggmlv3.q4_0.bin",
+        "model": "Wizard-Vicuna-7B-Uncensored.ggmlv3.q4_1.bin",
         "prompt": prompt_input,
         "temperature": temp.parse::<f64>().unwrap(),
         "stream": false,
