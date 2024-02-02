7a65e45c1280dbdc81c72a2d51b3a92193ed3343
Joel B. <smol@toast.cyou>
2024-02-02 16:47:55 -0500

    new method

--- reboot.sh
+++ reboot.sh
@@ -8,6 +8,6 @@ cargo build --release
 
 strip target/release/egghead
 
-screen -dmS my_screen_name /home/toast/llama.cpp/server -m ~/.ai/dolphin-2_6-phi-2-GGUF/dolphin-2_6-phi-2.Q4_K_M.gguf
+# screen -dmS llama-server /home/toast/llama.cpp/server -m ~/.ai/dolphin-2_6-phi-2-GGUF/dolphin-2_6-phi-2.Q4_K_M.gguf
 
 ./target/release/egghead
