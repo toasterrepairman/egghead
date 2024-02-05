6be06bd8ea06dc177435ef36cdeb6af0c343db12
Joel B. <smol@toast.cyou>
2024-02-05 15:45:41 -0500

    mlock enabled

--- reboot.sh
+++ reboot.sh
@@ -8,6 +8,6 @@ cargo build --release
 
 strip target/release/egghead
 
-# screen -dmS llama-server /home/toast/llama.cpp/server -m ~/.ai/Tess-XS-v1-3-yarn-128K-Mistral-7B-Instruct-v0.1.Q3_K_M.gguf -t 4
+# screen -dmS llama-server /home/toast/llama.cpp/server -m ~/.ai/Tess-XS-v1-3-yarn-128K-Mistral-7B-Instruct-v0.1.Q3_K_M.gguf -t 4 --mlock
 
 ./target/release/egghead
