d3555ffcd0785a62928221f8ee606e83b73ae66d
Joel B. <smol@toast.cyou>
2024-02-05 15:00:01 -0500

    new prompt

--- reboot.sh
+++ reboot.sh
@@ -8,6 +8,6 @@ cargo build --release
 
 strip target/release/egghead
 
-# screen -dmS llama-server /home/toast/llama.cpp/server -m ~/.ai/dolphin-2_6-phi-2-GGUF/dolphin-2_6-phi-2.Q4_K_M.gguf -t 2
+# screen -dmS llama-server /home/toast/llama.cpp/server -m ~/.ai/Truthful_DPO_TomGrc_FusionNet_7Bx2_MoE_13B-Q3_K_S.gguf -t 2
 
 ./target/release/egghead
