76a8c0be18ce5fdedc5f46ce5fdb997cfd3248fb
Joel B. <smol@toast.cyou>
2024-02-07 22:21:43 -0500

    new stats

--- reboot.sh
+++ reboot.sh
@@ -8,6 +8,6 @@ cargo build --release
 
 strip target/release/egghead
 
-# screen -dmS llama-server /home/toast/llama.cpp/server -m ~/.ai/Tess-XS-v1-3-yarn-128K-Mistral-7B-Instruct-v0.1.Q3_K_M.gguf -t 4 --mlock
+# screen -dmS llama-server /home/toast/llama.cpp/server -m ~/.ai/WestLake-7B-v2-laser-truthy-dpo.q2_k.gguf -t 4 --mlock
 
 ./target/release/egghead
