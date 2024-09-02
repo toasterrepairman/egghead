5acec9497da66775f234910bbb02f377f70c9381
Joel B. <smol@toast.cyou>
2024-09-02 15:29:23 -0400

    new generator

--- src/imgread.rs
+++ src/imgread.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use base64;
 
-fn base64_encode_image_file(file_path: &str) -> Result<String, std::io::Error> {
+pub fn base64_encode_image_file(file_path: &str) -> Result<String, std::io::Error> {
     let mut f = File::open(file_path)?;
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
