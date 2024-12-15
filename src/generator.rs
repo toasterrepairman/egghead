e04c2d046c0d7549a17a760dbd43dac6bdb2afd9
Joel B. <smol@toast.cyou>
2024-12-15 14:13:29 -0500

    smollm2 rewrite

--- src/generator.rs
+++ src/generator.rs
@@ -17,7 +17,7 @@ pub fn get_chat_response(temp: &str, init: &str, prompt: &str, imagedata: Option
 
     let prompt_input = format!("{}{}\n\n", init, prompt);
     let request_data = json!({
-        "model": "smollm:360m",
+        "model": "smollm2:135m",
         "prompt": prompt_input,
         // "temperature": temp.parse::<f64>().unwrap(),
         "stream": false,
