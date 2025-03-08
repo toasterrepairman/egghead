241890ff8253b688be6f32f66d565b933fe02111
Joel B. <smol@toast.cyou>
2025-03-07 21:43:40 -0500

    update readme

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
 # egghead
-*the world's smartest robot** 
+*the world's smartest robot**
 
 ## running
-On x86/Nix systems, you only need to run `nix develop .` in the root to generate a suitable compilation environment. 
-
-AArch64 build will use the same instructions, but require a native ARM-enabled PyTorch runtime.
+On x86/Nix systems, you only need to run `nix develop .` in the root to generate a suitable compilation environment.
 
 ### env
 
 All systems require the following environment variables to run.
 
 ```bash
-export LIBTORCH=$(python3 -c 'import torch; from pathlib import Path; print(Path(torch.__file__).parent)')
-export DYLD_LIBRARY_PATH=${LIBTORCH}/lib
-export LIBTORCH_CXX11_ABI=0
-export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
 export DISCORD_TOKEN=YOUR-TOKEN-HERE
 ```
- 
+
 *Not actually worldly, smart or a robot (technically).
--- flake.nix
+++ flake.nix
@@ -4,41 +4,44 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs:
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  } @ inputs:
     utils.lib.eachDefaultSystem
-      (system:
-        let
-          name = "egghead";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        rec {
-          packages.${name} = pkgs.callPackage ./default.nix {
-            inherit (inputs);
-          };
+    (
+      system: let
+        name = "egghead";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages.${name} = pkgs.callPackage ./default.nix {
+          inherit (inputs);
+        };
 
-          # `nix build`
-          defaultPackage = packages.${name};
+        # `nix build`
+        defaultPackage = packages.${name};
 
-          # `nix run`
-          apps.${name} = utils.lib.mkApp {
-            inherit name;
-            drv = packages.${name};
-          };
-          defaultApp = packages.${name};
+        # `nix run`
+        apps.${name} = utils.lib.mkApp {
+          inherit name;
+          drv = packages.${name};
+        };
+        defaultApp = packages.${name};
 
-          # `nix develop`
-          devShells = {
-            default = pkgs.mkShell {
-              nativeBuildInputs =
-                with pkgs; [
-                  rustc
-                  cargo
-                  openssl
-                  pkg-config
-                  git
-                ];
-            };
+        # `nix develop`
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              openssl
+              pkg-config
+              git
+            ];
           };
-        }
-      );
+        };
+      }
+    );
 }
