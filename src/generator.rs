98f98974fdeee951312bd9061a729a28ac75026c
Joel B. <smol@toast.cyou>
2025-08-16 16:58:41 -0400

    no no_think

--- src/generator.rs
+++ src/generator.rs
@@ -18,7 +18,7 @@ pub fn get_chat_response(temp: &str, init: &str, prompt: &str, imagedata: Option
     let prompt_input = format!("{}{}\n\n", init, prompt);
     let request_data = json!({
         "model": "gemma3:270m",
-        "prompt": format!("{}/no_think", prompt_input),
+        "prompt": format!("{}", prompt_input),
         // "temperature": temp.parse::<f64>().unwrap(),
         "stream": false,
         "stop": ["\n"],
