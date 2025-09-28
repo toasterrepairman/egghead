3526462b18c37fd00f52348b19b92ed481a12806
Joel B. <smol@toast.cyou>
2025-09-27 22:28:22 -0400

    better model name

--- src/generator.rs
+++ src/generator.rs
@@ -17,7 +17,7 @@ pub fn get_chat_response(temp: &str, init: &str, prompt: &str, imagedata: Option
 
     let prompt_input = format!("{}{}\n\n", init, prompt);
     let request_data = json!({
-        "model": "gemma3-abliterated:270m",
+        "model": "huihui_ai/gemma3-abliterated:270m",
         "prompt": format!("{}", prompt_input),
         // "temperature": temp.parse::<f64>().unwrap(),
         "stream": false,
